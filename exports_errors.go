5c86ad35c2fa937647bfac2fea5596795b918325
pcdogyu <pcdogyu@hotmail.com>
2026-04-24 14:48:17 +0800

    split page and shared exports by concern

--- exports_errors.go
+++ exports_errors.go
@@ -1,12 +1,5 @@
 package liqmap
 
-type ModelConfigPageData struct {
-	ModelConfig
-	PageTitle        string
-	ActiveMenu       string
-	ShowAnalysisInfo bool
-}
-
 type BadRequestError struct {
 	Message string
 }
