d1512d0e57e7bba50971c63f860028e4684e74d9
pcdogyu <pcdogyu@hotmail.com>
2026-04-24 15:16:20 +0800

    split app service wrappers by module

---
+++ app_service_analysis.go
@@ -0,0 +1,5 @@
+package liqmap
+
+func (a *App) BuildAnalysisSnapshot() (AnalysisSnapshot, error) {
+	return a.buildAnalysisSnapshot()
+}
--- app_service_channel.go
+++ app_service_channel.go
@@ -23,15 +23,3 @@ func (a *App) ListChannelTimeline(hours int) ([]ChannelTimelineRow, error) {
 func (a *App) ListChannelPlannedPushes(hours int) []ChannelPlannedPushRow {
 	return a.listChannelPlannedPushes(hours)
 }
-
-func (a *App) LoadModelConfig() ModelConfig {
-	return a.loadModelConfig()
-}
-
-func (a *App) SaveModelConfig(req ModelConfig) error {
-	return a.saveModelConfig(req)
-}
-
-func (a *App) RunModelFit(hours, minEvents int, exchange, mode string) (map[string]any, error) {
-	return a.runModelFit(hours, minEvents, exchange, mode)
-}
